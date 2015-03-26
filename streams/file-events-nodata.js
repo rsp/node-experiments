9687edc44ca72fed962d919f14bd1b1b12030c27
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-03-26 23:42:06 +0100

    Add file-events-all.js and file-events-nodata.js

---
+++ streams/file-events-nodata.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+// a test of Node.js stream events
+// from node-experiments by Rafał Pocztarski
+// See: https://github.com/rsp/node-experiments
+
+// This experiment shows which file events are fired
+// with no 'data' event handler.
+
+"use strict";
+
+var fs = require('fs');
+
+var num = 0;
+function logEvent(message) {
+	console.log(++num + '. ' + message);
+}
+
+var file = process.argv[2];
+
+if (file === undefined) {
+	console.log("argument missing");
+	process.exit(1);
+}
+
+console.log(
+	"file '" + file + "' stream events without 'data' event handler:"
+);
+
+var stream = fs.createReadStream(file);
+
+stream.on('open',     function () { logEvent('file open'); });
+stream.on('end',      function () { logEvent('file end'); });
+stream.on('close',    function () { logEvent('file close'); });
+//stream.on('data',     function () { logEvent('file data'); });
+stream.on('readable', function () { logEvent('file readable'); });
+stream.on('error',    function () { logEvent('file error'); });
+
