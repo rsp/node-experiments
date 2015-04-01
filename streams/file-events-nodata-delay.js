c5ed3d08d64e46dccdd23a1204e07d79723c8107
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-04-02 00:02:28 +0200

    Add file-events-nodata-delay.js

---
+++ streams/file-events-nodata-delay.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+// a test of Node.js stream events
+// from node-experiments by Rafał Pocztarski
+// See: https://github.com/rsp/node-experiments
+
+// This experiment shows which file events are fired
+// with no 'data' event handler.
+
+"use strict";
+
+var fs = require('fs');
+
+var num = 0;
+function logEvent(message) {
+	console.log(++num + '. ' + message);
+}
+
+var file = process.argv[2];
+
+if (file === undefined) {
+	console.log("argument missing");
+	process.exit(1);
+}
+
+console.log(
+	"file '" + file + "' stream events without 'data' event handler, delayed:"
+);
+
+var stream = fs.createReadStream(file);
+
+setTimeout(function () {
+
+	stream.on('open',     function () { logEvent('file open'); });
+	stream.on('end',      function () { logEvent('file end'); });
+	stream.on('close',    function () { logEvent('file close'); });
+	//stream.on('data',     function () { logEvent('file data'); });
+	stream.on('readable', function () { logEvent('file readable'); });
+	stream.on('error',    function () { logEvent('file error'); });
+
+}, 1000);
+
